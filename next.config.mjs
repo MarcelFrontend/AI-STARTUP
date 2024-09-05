08234886a1d071b9c99209fe4a2dde7e01f4b629
Adam <marceltomasz1806@gmail.com>
2024-09-05 12:48:16 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    // Dodaj obsługę plików SVG jako komponentów React
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // wyklucz *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/pages/sections/Features.tsx
@@ -0,0 +1,186 @@
+"use client";
+import {
+  DotLottieCommonPlayer,
+  DotLottiePlayer,
+} from "@dotlottie/react-player";
+import productImage from "../../assets/product-image.png";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
+import {
+  animate,
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  ValueAnimationTransition,
+} from "framer-motion";
+
+const tabs = [
+  {
+    icon: "/assets/lottie/vroom.lottie",
+    title: "User-friendly dashboard",
+    isNew: false,
+    backgroundPositionX: 0,
+    backgroundPositionY: 0,
+    backgroundSizeX: 150,
+  },
+  {
+    icon: "/assets/lottie/click.lottie",
+    title: "One-click optimization",
+    isNew: false,
+    backgroundPositionX: 98,
+    backgroundPositionY: 100,
+    backgroundSizeX: 135,
+  },
+  {
+    icon: "/assets/lottie/stars.lottie",
+    title: "Smart keyword generator",
+    isNew: true,
+    backgroundPositionX: 100,
+    backgroundPositionY: 27,
+    backgroundSizeX: 177,
+  },
+];
+
+const FeatureTab = (
+  props: (typeof tabs)[number] &
+    ComponentPropsWithoutRef<"div"> & { selected: boolean }
+) => {
+  const tabRef = useRef<HTMLDivElement>(null);
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+  const xPerc = useMotionValue(0);
+  const yPerc = useMotionValue(0);
+
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPerc}% ${yPerc}%, black, transparent)`;
+
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null) return;
+    dotLottieRef.current.seek(0);
+    dotLottieRef.current.play();
+  };
+
+  useEffect(() => {
+    if (!tabRef.current || !props.selected) return;
+
+    xPerc.set(0);
+    yPerc.set(0);
+    const { height, width } = tabRef.current?.getBoundingClientRect();
+
+    const circumference = height * 2 + width * 2;
+    const times = [
+      0,
+      width / circumference,
+      (width + height) / circumference,
+      (width * 2 + height) / circumference,
+      1,
+    ];
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 4,
+      repeat: Infinity,
+      ease: "linear",
+      repeatType: "loop",
+    };
+    animate(xPerc, [0, 100, 100, 0, 0], options);
+    animate(yPerc, [0, 0, 100, 100, 0], options);
+  }, [props.selected]);
+
+  return (
+    <div
+      ref={tabRef}
+      onMouseEnter={handleTabHover}
+      onClick={props.onClick}
+      className="border border-white/15 flex p-2.5 gap-2.5 items-center lg:flex-1 rounded relative"
+    >
+      {props.selected && (
+        <motion.div
+          className="absolute inset-0 -m-px border border-[#a369ff] rounded"
+          style={{ maskImage }}
+        ></motion.div>
+      )}
+      <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+        <DotLottiePlayer
+          ref={dotLottieRef}
+          src={props.icon}
+          className="w-5 h-5"
+          autoplay
+        />
+      </div>
+      <div className="font-medium">{props.title}</div>
+      {props.isNew && (
+        <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">
+          New
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const Features = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const bgPosX = useMotionValue(tabs[0].backgroundPositionX);
+  const bgPosY = useMotionValue(tabs[0].backgroundPositionY);
+  const bgSizeX = useMotionValue(tabs[0].backgroundSizeX);
+
+  const bgPos = useMotionTemplate`${bgPosX}% ${bgPosY}%`;
+  const bgSize = useMotionTemplate`${bgSizeX}% auto`;
+
+  const handleSelectTab = (index: number) => {
+    setSelectedTab(index);
+
+    const animateOptions: ValueAnimationTransition = {
+      duration: 2,
+      ease: "easeInOut",
+    };
+
+    animate(
+      bgSizeX,
+      [bgSizeX.get(), 100, tabs[index].backgroundSizeX],
+      animateOptions
+    );
+
+    animate(
+      bgPosX,
+      [bgPosX.get(), tabs[index].backgroundPositionX],
+      animateOptions
+    );
+
+    animate(
+      bgPosY,
+      [bgPosY.get(), tabs[index].backgroundPositionY],
+      animateOptions
+    );
+  };
+
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <h3 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">
+          Elevate your SEO efforts.
+        </h3>
+        <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
+          From small startups to large enterprise, our AI-driven tool has
+          revolutionized the way businesses aproach SEO.
+        </p>
+        <div className="mt-10 flex flex-col lg:flex-row gap-3">
+          {tabs.map((tab, tabIndex) => (
+            <FeatureTab
+              {...tab}
+              selected={selectedTab === tabIndex}
+              key={tab.title}
+              onClick={() => handleSelectTab(tabIndex)}
+            />
+          ))}
+        </div>
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+          <motion.div
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
+            style={{
+              backgroundPosition: bgPos,
+              backgroundSize: bgSize,
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          ></motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
